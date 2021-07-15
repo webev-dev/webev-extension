6979bdb70d296bbad6ff2e6d042fdbe0c2191e11
Shun Miyazawa <shun-m@weseek.co.jp>
2021-07-15 13:51:54 +0900

    add TODO

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const Options = () => {
+  const onClickDeleteAccessTokenButton = () => {
+    // TODO: accessToken を破棄する為の処理を書く
+    console.log('deleteAccessTokenButton clicked');
+  };
+
   return (
     <>
       <p>機能一覧</p>
@@ -8,6 +13,7 @@ const Options = () => {
         <li>ページの url を webev に保存できます</li>
       </ul>
       <a href="https://itizawa-tech.growi.cloud/projects/webev/%E6%96%BD%E7%AD%96/www.webev.cloud/inquery" target="_blank" rel="noreferrer">お問い合わせ</a>
+      <button onClick={onClickDeleteAccessTokenButton}>accessToken を削除する</button>
     </>
   );
 };
